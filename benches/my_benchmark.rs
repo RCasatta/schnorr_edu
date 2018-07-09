8e4067cc058dff0f22bb280380336b3b23e29f5f
Riccardo Casatta <riccardo@casatta.it>
2018-07-09 14:41:44 +0200

    fix benches

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -9,27 +9,15 @@ use criterion::Criterion;
 use schnorr_edu::*;
 use num_bigint::BigUint;
 
-
-fn criterion_benchmark(c: &mut Criterion) {
-    let mut rng = thread_rng();
-    let mut msg = [0u8;32];
-    let mut sec_key = [0u8;32];
-    let context = Context::default();
-    c.sample_size(2);
-    c.bench_function("Schnorr sign",move |b|
-        b.iter(|| {
-            rng.fill_bytes(&mut msg);
-            rng.fill_bytes(&mut sec_key);
-        schnorr_sign(&msg,&sec_key,&context);
-        } ));
-
+fn benchmark_verify(c: &mut Criterion) {
     let context = Context::default();
     let mut rng = thread_rng();
     let msg = [0u8;32];
     let mut sec_key = [0u8;32];
     let mut signatures = Vec::new();
     let mut pub_keys = Vec::new();
-    for _ in 0..1000 {
+    let precomputed_signatures= 100usize;
+    for _ in 0..precomputed_signatures {
         rng.fill_bytes(&mut sec_key);
         let signature = schnorr_sign(&msg,&sec_key,&context);
         let sec_key_int = BigUint::from_bytes_be(&sec_key);
@@ -39,11 +27,31 @@ fn criterion_benchmark(c: &mut Criterion) {
     }
 
     c.bench_function("Schnorr verify",move |b| b.iter(|| {
-        let i = rng.gen_range(0usize, 1000usize);
+        let i = rng.gen_range(0usize, precomputed_signatures);
         let result = schnorr_verify(&msg, &pub_keys[i], &signatures[i], &context);
         assert!(result);
     } ));
 }
 
-criterion_group!(benches, criterion_benchmark);
+fn benchmark_sign(c: &mut Criterion) {
+    let mut rng = thread_rng();
+    let mut msg = [0u8;32];
+    let mut sec_key = [0u8;32];
+    let context = Context::default();
+    c.bench_function("Schnorr sign",move |b|
+        b.iter(|| {
+            rng.fill_bytes(&mut msg);
+            rng.fill_bytes(&mut sec_key);
+        schnorr_sign(&msg,&sec_key,&context);
+        } ));
+
+
+}
+
+criterion_group!{
+    name = benches;
+    config = Criterion::default().sample_size(2);
+    targets = benchmark_verify, benchmark_sign
+}
+
 criterion_main!(benches);
